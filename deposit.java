136a6fd3c8f8d0e05b602fb1a2c091838b1f99f8
Naveen131298 <51261901+Naveen131298@users.noreply.github.com>
2021-10-31 22:44:30 +0530

    Update deposit.java

--- deposit.java
+++ deposit.java
@@ -2,74 +2,73 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
-class deposit implements ActionListener
+class withdraw implements ActionListener
 {
 	JFrame f;
 	JPanel p;
 	JLabel ld,ld1,ld2,ld3;
 	JTextField td1,td2,td3;
-	JButton b1,b2,b3,b4,b5,b6,b7;
-	deposit()
+	JButton b1,b3,b7;
+	withdraw()
 	{
-		f=new JFrame("DEPOSIT");
-		f.setBackground(Color.white);
-		p=new ImagePanel(new ImageIcon("bankgold.jpeg").getImage());
+		f=new JFrame();
+		p=new ImagePanel(new ImageIcon("currency.jpg").getImage());
 		f.getContentPane().add(p);
 		f.setVisible(true);
 		f.setSize(500,500);
 		p.setLayout(null);
-		ld=new JLabel("DEPOSIT ");
+		ld=new JLabel(" WITHDRAWAL ");
 		Font fd=new Font("Bookman Old Style",Font.BOLD,30);
 		ld.setFont(fd);
-
-		Font fd1=new Font("Lucida Handwriting",Font.BOLD,16);
 		ld1=new JLabel("Account No:");
+		Font fd1=new Font("Lucida Handwriting",Font.BOLD,20);
 		ld1.setFont(fd1);
+		ld1.setForeground(Color.black);
 		td1=new JTextField(30);
 		ld2=new JLabel("Current Balance:");
 		ld2.setFont(fd1);
 		td2=new JTextField(30);
-		ld3=new JLabel("Deposit Amount:");
+		ld3=new JLabel("Withdraw Amount:");
 		ld3.setFont(fd1);
 		td3=new JTextField(30);
 
-		b1=new JButton("DEPOSIT");
+		b1=new JButton("WITHDRAW");
 		b1.setFont(fd1);
 		b1.addActionListener(this);
-		b2=new JButton("SAVE");
-		b2.setFont(fd1);
-		b2.addActionListener(this);
+		//b2=new JButton("SAVE");
+		//b2.setFont(fd1);
+		//b2.addActionListener(this);
 		b3=new JButton("SEARCH");
 		b3.setFont(fd1);
 		b3.addActionListener(this);
-		b4=new JButton("UPDATE");
-		b4.setFont(fd1);
-		b4.addActionListener(this);
-		b5=new JButton("DELETE");
-		b5.setFont(fd1);
-		b5.addActionListener(this);
-		b6=new JButton("RESET");
-		b6.setFont(fd1);
-		b6.addActionListener(this);
+		//b4=new JButton("UPDATE");
+		//b4.setFont(fd1);
+		//b4.addActionListener(this);
+		//b5=new JButton("DELETE");
+		//b5.addActionListener(this);
+		//b5.setFont(fd1);
+		//b6=new JButton("RESET");
+		//b6.addActionListener(this);
+		//b6.setFont(fd1);
 		b7=new JButton("BACK TO HOME PAGE");
 		b7.setFont(fd1);
 		b7.addActionListener(this);
 
-		ld.setBounds(375,100,500,30);
-		ld1.setBounds(270,200,300,30);
-		td1.setBounds(550,200,200,30);
-		ld2.setBounds(270,270,300,30);
-		td2.setBounds(550,270,200,30);
-		ld3.setBounds(270,340,300,30);
-		td3.setBounds(550,340,200,30);
+		ld.setBounds(830,100,500,30);
+		ld1.setBounds(700,200,300,30);
+		td1.setBounds(980,200,200,30);
+		ld2.setBounds(700,270,300,30);
+		td2.setBounds(980,270,200,30);
+		ld3.setBounds(700,340,300,30);
+		td3.setBounds(980,340,200,30);
 
-		b1.setBounds(300,450,150,30);
-		b2.setBounds(580,500,150,30);
-		b3.setBounds(500,450,150,30);
-		b4.setBounds(940,500,150,30);
-		b5.setBounds(1120,500,150,30);
-		b6.setBounds(750,580,150,30);
-		b7.setBounds(325,500,300,30);
+		b1.setBounds(730,450,200,30);
+		//b2.setBounds(580,500,150,30);
+		b3.setBounds(980,450,200,30);
+		//b4.setBounds(940,500,150,30);
+		//b5.setBounds(1120,500,150,30);
+		//b6.setBounds(750,580,150,30);
+		b7.setBounds(800,500,350,30);
 
 		p.add(ld);
 		p.add(ld1);
@@ -79,12 +78,13 @@ class deposit implements ActionListener
 		p.add(ld3);
 		p.add(td3);
 		p.add(b1);
-	
+		//p.add(b2);
 		p.add(b3);
+		//p.add(b4);
+		//p.add(b5);
+		//p.add(b6);
 		p.add(b7);
-		
 	}
-		
 	public void actionPerformed(ActionEvent ae)
 	{
 		try
@@ -94,18 +94,18 @@ class deposit implements ActionListener
 			Statement s=c.createStatement();
 			ResultSet rs;
 			
-		if(ae.getSource()==b1)//calculate
+		if(ae.getSource()==b1)//withdraw
 		{
 			int acc=Integer.parseInt(td2.getText());
-			int deposit=Integer.parseInt(td3.getText());
+			int withdraw=Integer.parseInt(td3.getText());
 			int x;
-			x=acc+deposit;
+			x=acc-withdraw;
 			td2.setText(""+x);
 			td3.setText("");
 			
 		}
 
-		if(ae.getSource()==b2)//insert
+		/*if(ae.getSource()==b2)//insert
 		{
 
 			int r=s.executeUpdate("insert into wtable values('"+td1.getText()+"','"+td2.getText()+"')");
@@ -117,7 +117,7 @@ class deposit implements ActionListener
 			td1.setText("");
 			td2.setText("");
 			
-		}
+		}*/
 
 		if(ae.getSource()==b3)//search
 		{
@@ -135,7 +135,7 @@ class deposit implements ActionListener
 			td1.setText("");
 			}
 		}
-		if(ae.getSource()==b4) //update
+		/*if(ae.getSource()==b4) //update
 		{
 			int r=s.executeUpdate("update wtable set account_no='"+td1.getText()+"',current_balance='"+td2.getText()+"' where account_no='"+td1.getText()+"'");
 			if(r!=0)
@@ -168,7 +168,7 @@ class deposit implements ActionListener
 			td2.setText("");
 			td3.setText("");
 			
-		}
+		}*/
 		if(ae.getSource()==b7) //close
 		{
 			new home();
@@ -181,10 +181,10 @@ class deposit implements ActionListener
 			JOptionPane.showMessageDialog(f,e);
 	}
           }	
-		
 	public static void main(String ar[])
 	{
-		new deposit();
+		new withdraw();
 	}
 }
+	
 	
